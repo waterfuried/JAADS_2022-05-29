44c9f678ef2c7adde3933ace7070cc5e1efc4770
waterfuried <cisternsystem@gmail.com>
2022-06-15 20:53:49 +0300

    задача 7. реализация.

--- src/task07/Stack.java
+++ src/task07/Stack.java
@@ -14,7 +14,14 @@ class Stack {
 
     public boolean isEmpty() { return top == -1; }
 
+    public boolean hasElements() { return top >= 0; }
+
     public void push(int i) {
+        if (top == maxSize - 1) {
+            int[] tmp = new int[maxSize <<= 1];
+            System.arraycopy(stackArr, 0, tmp, 0, stackArr.length);
+            stackArr = tmp;
+        }
         stackArr[++top] = i;
     }
 
@@ -24,5 +31,39 @@ class Stack {
 
     public int peek() {
         return stackArr[top];
+    }
+
+    @Override public String toString() {
+        if (isEmpty()) return "";
+        StringBuilder sb = new StringBuilder();
+        int t = top;
+        while (t >= 0) sb.append(" ").append(stackArr[t--]);
+        return sb.toString();
+    }
+
+    public String toString(String separator, boolean asChar) {
+        if (isEmpty()) return "";
+        StringBuilder sb = new StringBuilder();
+        int t = top;
+        while (t >= 0) {
+            if (asChar)
+                sb.append((char)((int)'A'+stackArr[t]));
+            else
+                sb.append(stackArr[t]);
+            if (--t >= 0) sb.append(separator);
+        }
+        return sb.toString();
+    }
+
+    public void display(String ... s) { display(false, s); }
+
+    public void display(boolean asChar, String ... s) {
+        String str = null;
+        if (s != null && s.length > 0) {
+            str = s[0];
+            if (s.length > 1) str += this.toString(s[1], asChar);
+        }
+        if (str == null) str = this.toString();
+        System.out.println(str);
     }
 }
