a019ce0bbd9d6bda110166ba99efad8b18b063eb
waterfuried <cisternsystem@gmail.com>
2022-06-15 17:26:37 +0300

    задача 7.

---
+++ src/task07/Vertex.java
@@ -0,0 +1,12 @@
+package task07;
+
+// класс вершины
+class Vertex {
+    public char label;
+    public boolean wasVisited;
+
+    public Vertex(char label) {
+        this.label = label;
+        this.wasVisited = false;
+    }
+}
