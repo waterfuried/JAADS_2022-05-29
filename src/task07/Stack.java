4bcc4e784e5933677902fb213f2682b193b81c91
waterfuried <cisternsystem@gmail.com>
2022-06-15 20:13:58 +0300

    задача 7.

---
+++ src/task07/Stack.java
@@ -0,0 +1,28 @@
+package task07;
+
+// класс стека
+class Stack {
+    private int maxSize;
+    private int[] stackArr;
+    private int top;
+
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArr = new int[size];
+        this.top = -1;
+    }
+
+    public boolean isEmpty() { return top == -1; }
+
+    public void push(int i) {
+        stackArr[++top] = i;
+    }
+
+    public int pop() {
+        return stackArr[top--];
+    }
+
+    public int peek() {
+        return stackArr[top];
+    }
+}
