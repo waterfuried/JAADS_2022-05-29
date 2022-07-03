4347e329caf21d80e20747beda21ce3b5ae2b52a
waterfuried <cisternsystem@gmail.com>
2022-07-03 19:57:10 +0300

    задача 8. метод цепочек. реализация.

--- src/task08/Item.java
+++ src/task08/Item.java
@@ -2,10 +2,15 @@ package task08;
 
 class Item {
   private int data;
+  private Item next;
 
   public Item(int data) {
     this.data = data;
+    this.next = null;
   }
 
-  public int getKey() { return this.data; }
+  public int getKey() { return data; }
+  public void setKey(int data) { this.data = data; }
+  public Item getNext() { return next; }
+  public void setNext(Item next) { this.next = next; }
 }
